9bea3343ac5f988a3b49d28b414e6a8ccab58667
NigelAitch <154019127+NigelAitch@users.noreply.github.com>
2025-07-09 08:55:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -35,14 +35,12 @@ app.use(
   })
 );
 
-// Add this middleware to handle CORS properly
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://main.d2e1ko68ec6usk.amplifyapp.com');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', 'true');
   
-  // Handle preflight requests
   if (req.method === 'OPTIONS') {
     res.sendStatus(200);
   } else {
