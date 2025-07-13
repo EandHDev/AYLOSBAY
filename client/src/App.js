68ad5c0ef07e84fa8611be7bd5c3c328595f97fa
NigelAitch <nigel@ianaitch.com>
2025-07-12 21:49:16 -0400

    Homepage added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Homescreen from "./screens/Homescreen";
 import AddRoom from "./screens/AddRoom";
 import AdminRoom from "./screens/AdminRoom";
 import BookingScreen from "./screens/BookingScreen";
+import AylosBayHomepage from "./components/AylosBayHomepage";
 import "bootstrap/dist/css/bootstrap.min.css";
 import RegisterScreen from "./screens/RegisterScreen";
 import LoginScreen from "./screens/LoginScreen";
@@ -42,22 +43,49 @@ const RouteWithErrorBoundary = ({ element }) => (
   <ErrorBoundary>{element}</ErrorBoundary>
 );
 
+// Component to conditionally show Navbar
+const ConditionalNavbar = () => {
+  const location = useLocation();
+
+  // Don't show Navbar on homepage
+  if (location.pathname === "/") {
+    return null;
+  }
+
+  return (
+    <ErrorBoundary>
+      <Navbar />
+    </ErrorBoundary>
+  );
+};
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <ErrorBoundary>
-          <Navbar />
-        </ErrorBoundary>
+        <ConditionalNavbar />
         <Routes>
+          {/* NEW: Luxury homepage as landing page */}
           <Route
             path="/"
+            element={<RouteWithErrorBoundary element={<AylosBayHomepage />} />}
+          />
+
+          {/* UPDATED: Move booking system to /booking and /rooms */}
+          <Route
+            path="/booking"
+            element={<RouteWithErrorBoundary element={<Homescreen />} />}
+          />
+          <Route
+            path="/rooms"
             element={<RouteWithErrorBoundary element={<Homescreen />} />}
           />
           <Route
             path="/home"
             element={<RouteWithErrorBoundary element={<Homescreen />} />}
           />
+
+          {/* Keep all existing routes */}
           <Route
             path="/admin/addroom"
             element={<RouteWithErrorBoundary element={<AddRoom />} />}
@@ -72,7 +100,6 @@ function App() {
           />
           <Route path="/admin" element={<AdminPanel />} />
           <Route path="/profile" element={<UserDashboard />} />
-          {/* --- END ADDITION --- */}
           <Route path="/register" element={<RegisterScreen />} />
           <Route path="/login" element={<LoginScreen />} />
           <Route
